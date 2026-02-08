47d4154e9c2bcfd7c3911205a7317ee4139ae835
GamerFoxy0 <azadkucukaslan@gmail.com>
2026-02-09 01:15:43 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,2 +1,5 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/AsuraXowner/SentinelVAPE/refs/heads/main/main.lua", true))()
---i remove everything for bunni shit executor work
+if isfolder("sentinelvape") and not isfile("sentinelvape/main.lua") then
+   writefile("sentinelvape/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelVAPE/refs/heads/main/main.lua", true))
+elseif isfolder("sentinelvape") and isfile("sentinelvape/main.lua") then
+    loadstring(readfile("sentinelvape/main.lua"))()
+end
