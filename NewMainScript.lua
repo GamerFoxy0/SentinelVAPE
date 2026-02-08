7ff5e0945652b2f43b2dcb22156332674e5a5df3
GamerFoxy0 <azadkucukaslan@gmail.com>
2026-02-09 01:25:43 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,5 +1,9 @@
+if not isfolder("sentinelvape") then
+    makefolder("sentinelvape")
+end
 if isfolder("sentinelvape") and not isfile("sentinelvape/main.lua") then
-   writefile("sentinelvape/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelVAPE/refs/heads/main/main.lua", true))
+    writefile("sentinelvape/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelVAPE/refs/heads/main/main.lua", true))
+    loadstring(readfile("sentinelvape/main.lua"))()
 elseif isfolder("sentinelvape") and isfile("sentinelvape/main.lua") then
     loadstring(readfile("sentinelvape/main.lua"))()
 end
