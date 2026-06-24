41cee15d7700319395908b3f96fd63740e15e6ee
GamerFoxy0 <azadkucukaslan@gmail.com>
2026-06-24 14:24:31 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,3 +1,13 @@
+if game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelVAPE/refs/heads/main/version.txt"):gsub("%s+", "") ~= "0.8.2" then
+     if isfolder("sentinelvape") then
+        delfolder("sentinelvape/games")
+        delfolder("sentinelvape/guis")
+        if isfile("sentinelvape/main.lua") then
+            delfile("sentinelvape/main.lua")
+        end
+    end
+end
+
 if not isfolder("sentinelvape") then
     makefolder("sentinelvape")
 end
