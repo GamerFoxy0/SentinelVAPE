21e936a9e658fa5c05d2570088c2e76ad7efaf29
GamerFoxy12 <pogyproguy@gmail.com>
2026-06-30 15:31:57 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -1,19 +1,5 @@
-if game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelVAPE/refs/heads/main/version.txt"):gsub("%s+", "") ~= "0.8.25" then
-     if isfolder("sentinelvape") then
-        delfolder("sentinelvape/games")
-        delfolder("sentinelvape/guis")
-        if isfile("sentinelvape/main.lua") then
-            delfile("sentinelvape/main.lua")
-        end
-    end
-end
-
-if not isfolder("sentinelvape") then
-    makefolder("sentinelvape")
-end
-if isfolder("sentinelvape") and not isfile("sentinelvape/main.lua") then
-    writefile("sentinelvape/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/SentinelVAPE/refs/heads/main/main.lua", true))
-    loadstring(readfile("sentinelvape/main.lua"))()
-elseif isfolder("sentinelvape") and isfile("sentinelvape/main.lua") then
-    loadstring(readfile("sentinelvape/main.lua"))()
-end
+local d = "sentinelvape"
+local u = "SentinelVAPE"
+local t, l = pcall(function() return game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/version.txt"):gsub("%s+", "") end), isfile(d.."/version.cfg") and readfile(d.."/version.cfg"):gsub("%s+", "") or ""
+if not t or l ~= game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/version.txt"):gsub("%s+", "") or not isfile(d.."/main.lua") then if isfolder(d) then pcall(delfolder, d.."/games") pcall(delfolder, d.."/guis") pcall(delfile, d.."/main.lua") end if not isfolder(d) then makefolder(d) end writefile(d.."/main.lua", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/main.lua", true)) writefile(d.."/version.cfg", game:HttpGet("https://raw.githubusercontent.com/GamerFoxy0/"..u.."/refs/heads/main/version.txt"):gsub("%s+", "")) end
+loadstring(readfile(d.."/main.lua"))()
